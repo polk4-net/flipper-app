eaa75bfb3d67f79b7ec682590000ec3758b84a82
Nick <mshulhin@gmail.com>
2021-07-04 18:22:10 +1000

    Origin

---
+++ README.md
@@ -0,0 +1,42 @@
+# ⛓ Substrate contract Web App interactor template - for stock flipper ⛓
+
+This project has everything you need to have an idea how to interact with your deployed Substrate contract.
+
+It uses compiled contract well known as flipper (`cargo contract new flipper`)
+
+Please substitute `src/change_with_your_own_metadata.json` with your own compiled flipper `metadata.json` (ABI)
+
+## Submit features/refactor
+
+Don't hesitate to submit pull-requests with proposed features/bug fixes if any.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`****
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `yarn test`
+
+Launches the test runner in the interactive watch mode.\
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+
+### `yarn build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+## License
+polk4-web is [MIT licensed](LICENSE)
+
---
+++ src/App.js
@@ -0,0 +1,110 @@
+import './App.css';
+import {ApiPromise, WsProvider} from '@polkadot/api';
+import {useState, useEffect} from "react";
+import {ContractPromise} from '@polkadot/api-contract';
+import abi from './change_with_your_own_metadata.json';
+import {web3Accounts, web3Enable, web3FromSource} from '@polkadot/extension-dapp';
+
+function App() {
+    const [block, setBlock] = useState(null);
+    const [actingAddress, setActingAddress] = useState(null);
+    const [lastBlockHash, setLastBlockHash] = useState(null);
+    const [api, setApi] = useState(null);
+    const [accounts, setAccounts] = useState([]);
+    const [result, setResult] = useState(null);
+    const [gasConsumed, setGasConsumed] = useState(null);
+    const [outcome, setOutcome] = useState(null);
+    const [contractAddress, setContractAddress] = useState(null);
+    const [blockchainUrl, setBlockchainUrl] = useState('wss://canvas.polk4.net');
+
+    // NOTE the apps UI specified these in mega units -> https://polkadot.js.org/docs/api-contract/start/contract.read
+    const gasLimit = 3000n * 1000000n;
+
+    // Read from the contract via an RPC call
+    const value = 0; // only useful on isPayable messages -> https://polkadot.js.org/docs/api-contract/start/contract.read
+
+    const extensionSetup = async () => {
+        const extensions = await web3Enable('Polk4NET');
+        if (extensions.length === 0) {
+            return;
+        }
+        setAccounts(await web3Accounts());
+    };
+
+    useEffect(() => {
+        setup();
+    }, []);
+
+
+    async function getFlipValue() {
+        const contract = new ContractPromise(api, abi, contractAddress);
+        const {gasConsumed, result, output} = await contract.query.get(actingAddress, {value, gasLimit});
+        setGasConsumed(gasConsumed.toHuman());
+        setResult(JSON.stringify(result.toHuman()));
+        setOutcome(output.toHuman().toString());
+    }
+
+    async function changeFlipValue() {
+        const contract = new ContractPromise(api, abi, contractAddress);
+        const performingAccount = accounts.filter(a => a.address === actingAddress)[0];
+        const injector = await web3FromSource(performingAccount.meta.source);
+        await contract.tx
+            .flip({value, gasLimit})
+            .signAndSend(performingAccount.address, {signer: injector.signer}, (result) => {
+                if (result.status.isInBlock) {
+                    setResult('in a block');
+                } else if (result.status.isFinalized) {
+                    setResult('finalized');
+                }
+            });
+    }
+
+    async function setup() {
+        const wsProvider = new WsProvider(blockchainUrl);
+        const api = await ApiPromise.create({provider: wsProvider});
+        await api.rpc.chain.subscribeNewHeads((lastHeader) => {
+            setBlock(`${lastHeader.number}`);
+            setLastBlockHash(`${lastHeader.hash}`);
+        });
+        setApi(api);
+        await extensionSetup();
+    }
+
+    return (
+        <div className="App">
+            <p>Polkadot test net well-known seed for Alice (Derive for any other if needed like Bob Eve etc.) use it in
+                extension to create a test account: "bottom drive obey lake curtain smoke basket hold race lonely fit
+                walk//Alice"</p>
+            <p>Block: {block}</p>
+            <p>Blockchain URL: {blockchainUrl}</p>
+            <p>Custom Blockchain URL</p>
+            <button onClick={setup}>Change Blockchain URL</button>
+            <input onChange={(event) => setBlockchainUrl(event.target.value)}/>
+            <p>Last block hash: {lastBlockHash}</p>
+            <p>Input contract address (from your canvas UI after you instantiate it): {contractAddress}</p>
+            <input onChange={(event) => setContractAddress(event.target.value)}/>
+            <br/>
+            <p>Acting account (select from dropdown): {actingAddress ? actingAddress : '...'}</p>
+            <br/>
+            <select onChange={(event) => {
+                console.log(event);
+                setActingAddress(event.target.value)
+            }}>
+                {accounts.map(a => <option value={a.address}>{a.address} [{a.meta.name}]</option>)}
+            </select>
+            <br/>
+            <br/>
+            <button disabled={!api || !contractAddress}
+                    onClick={getFlipValue}>{api && contractAddress ? 'Get flip value!' : 'Couldn\'t load API or contract address is invalid, please see logs in console.'}</button>
+            <br/>
+            <br/>
+            <button disabled={!api || !contractAddress}
+                    onClick={changeFlipValue}>{api && contractAddress ? 'Change flip value!' : 'Couldn\'t load API or contract address is invalid, please see logs in console.'}</button>
+            <p>Result: {result}</p>
+            <p>Outcome: {outcome}</p>
+            <p>Gas consumed: {gasConsumed}</p>
+        </div>
+    );
+}
+
+export default App;
